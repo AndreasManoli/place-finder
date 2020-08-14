f4572d88d85585fd6a3c6dc51de63c8f72615166
AndreasManoli <51517142+AndreasManoli@users.noreply.github.com>
2020-08-14 19:37:16 +0300

    Update

--- src/app/modules/google-map/compoents/google-map/google-map.component.ts
+++ src/app/modules/google-map/compoents/google-map/google-map.component.ts
@@ -1,5 +1,6 @@
-import { Component, Inject, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Inject, Input, ViewChild } from '@angular/core';
 import { MapInfoWindow, MapMarker } from '@angular/google-maps';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
 import * as _ from 'lodash-es';
 import { AppConfig } from 'src/app/interfaces/config.interface';
 import { LatLng } from 'src/app/modules/auto-complete/interfaces/latLng.interface';
@@ -10,7 +11,8 @@ import { MapsService } from '../../services/maps.service';
   templateUrl: './google-map.component.html',
   styleUrls: ['./google-map.component.scss']
 })
-export class GoogleMapComponent implements OnInit {
+@UntilDestroy()
+export class GoogleMapComponent {
   @ViewChild(MapInfoWindow) infoWindow: MapInfoWindow;
 
   @Input() set Center(value: LatLng) {
@@ -22,7 +24,7 @@ export class GoogleMapComponent implements OnInit {
 
   constructor(private mapsService: MapsService, @Inject(APP_CONFIG) private config: AppConfig) {}
 
-  data;
+  data: Partial<{}>;
   center: google.maps.LatLngLiteral;
 
   options: google.maps.MapOptions = {
@@ -31,23 +33,26 @@ export class GoogleMapComponent implements OnInit {
     minZoom: 8
   };
 
-  content: { name: string; photos: any; openNow?: boolean; profilePhoto: string; address: string; phone: string; rating: string } = null;
-  // Todo:
-  openInfoWindow(marker: MapMarker, item) {
-    this.content = null;
-    this.mapsService.getDetails(item.place_id).subscribe(x => {
-      this.content = {
-        name: x.name,
-        address: x.formatted_address,
-        profilePhoto: x.icon,
-        phone: x.formatted_phone_number,
-        rating: x.rating,
-        openNow: x?.opening_hours?.open_now,
-        photos: _.map(x.photos, z => this.config.urls.photo + z.photo_reference + '&key=' + this.config.general.apiKey)
-      };
-    });
-    this.infoWindow.open(marker);
-  }
+  content: { name: string; photos: any; openNow: boolean; profilePhoto: string; address: string; phone: string; rating: string } = null;
 
-  ngOnInit(): void {}
+  openInfoWindow = (marker: MapMarker, item: { place_id: string }): void => {
+    this.content = null;
+    this.mapsService
+      .getDetails(item.place_id)
+      .pipe(untilDestroyed(this))
+      .subscribe(x => {
+        this.content = {
+          name: x.name,
+          address: x.formatted_address,
+          profilePhoto: x.icon,
+          phone: x.formatted_phone_number,
+          rating: x.rating,
+          openNow: x?.opening_hours?.open_now,
+          photos: _.map(x.photos, z => this.config.urls.photo + z.photo_reference + '&key=' + this.config.general.apiKey)
+        };
+      });
+    this.infoWindow.open(marker);
+  };
+
+  trackById = (index: number, value: string): string => index + value;
 }
