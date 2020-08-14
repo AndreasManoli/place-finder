bfea93965d7d55a896a567839f6d481c80df5deb
AndreasManoli <51517142+AndreasManoli@users.noreply.github.com>
2020-08-14 22:35:45 +0300

    fix issue

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,12 @@ import { ApiKeyParamInterceptor } from './interceptors/api-key-param.interceptor
 import { GoogleMapModule } from './modules/google-map/google-map.module';
 import { SearchModule } from './modules/search/search.module';
 import { APP_CONFIG, PLACE_FINDER_CONFIG } from './providers/config.provider';
-import { SearchWrapperComponent } from './search-wrapper/search-wrapper.component';
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 @NgModule({
-  declarations: [AppComponent, SearchWrapperComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
